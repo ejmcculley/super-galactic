7fb75a85f405af2f523f370bd53157b654d6479d
ejmcculley <92233564+ejmcculley@users.noreply.github.com>
2022-01-02 18:15:43 -0800

    Add test for AgeCalculator.prototype.galacticAge

--- __tests__/age-calculator.test.js
+++ __tests__/age-calculator.test.js
@@ -1,13 +1,19 @@
+import { test } from "media-typer";
 import AgeCalculator from "./../src/age-calculator";
 
 describe("AgeCalculator", () => {
 
   test("should correctly create an object with five ages", () => {
-    const galacticAge = new AgeCalculator(33, 138, 54, 17, 2);
-    expect(galacticAge.earthAge).toEqual(33);
-    expect(galacticAge.mercuryAge).toEqual(138);
-    expect(galacticAge.venusAge).toEqual(54);
-    expect(galacticAge.marsAge).toEqual(17);
-    expect(galacticAge.jupiterAge).toEqual(2);
+    const ageCalculator = new AgeCalculator(33, 138, 54, 17, 2);
+    expect(ageCalculator.earthAge).toEqual(33);
+    expect(ageCalculator.mercuryAge).toEqual(138);
+    expect(ageCalculator.venusAge).toEqual(54);
+    expect(ageCalculator.marsAge).toEqual(17);
+    expect(ageCalculator.jupiterAge).toEqual(2);
+  });
+  test("should return earthAge when age is entered", () => {
+    const ageCalculator = new AgeCalculator(33, 138, 54, 17, 2);
+    expect(ageCalculator.galacticAge(age)).toEqual(earthAge);
   })
-})
+
+});
